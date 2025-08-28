e58018898bba628a6e096b1c970d6a463fc68ab2
Senecal <ssenecal@una.edu>
2025-08-28 12:40:56 -0500

    update

--- app.mjs
+++ app.mjs
@@ -2,8 +2,15 @@ import express from 'express'
 
 const app = express()
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
-  res.send('I Fucking Hate Jacob')
+  res.send('Hello Express from Render')
 })
 
+// endpoints...middlewares...apis'
+// send an html file
+
 app.listen(3000)
+
+// TODO: refactor to use any port
