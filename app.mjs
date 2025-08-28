45ec553f35f471659078b8ed4ce2c3238d9f1554
Senecal <ssenecal@una.edu>
2025-08-28 12:42:59 -0500

    refactor

--- app.mjs
+++ app.mjs
@@ -1,16 +1,28 @@
 import express from 'express'
 
 const app = express()
-
 const PORT = process.env.PORT || 3000;
+const path = require()
+
+app.use(express.static(_dirname + 'public'));
 
 app.get('/', (req, res) => {
-  res.send('Hello Express from Render')
+  res.send('Hello Express from Render. <a href="/samuel</a>')
 })
 
 // endpoints...middlewares...apis'
 // send an html file
 
-app.listen(3000)
+app.get('/samuel', (req, res) => {
+  // res.send('samuel. <a href="/')
+  res.sendFile('samuel.html');
+})
+
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
 
 // TODO: refactor to use any port
+
