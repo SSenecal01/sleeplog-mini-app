a7db2e1698933eccfdccbc7dfead921d7ca6cbcf
Senecal <ssenecal@una.edu>
2025-09-02 12:42:12 -0500

    refactor

--- app.mjs
+++ app.mjs
@@ -19,13 +19,9 @@ app.get('/', (req, res) => {
 // send an html file
 
 app.get('/samuel', (req, res) => {
-  // res.send('samuel. <a href="/')
-  res.sendFile(path.join(__dirname, 'public', 'samuel.html'));
+  res.sendFile(path.join(__dirname, 'public', 'samuel.html'))
 })
 
-//app.get('/api/samuel', (req, res) => {
-// const myVar = "Howdy";
-//})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
