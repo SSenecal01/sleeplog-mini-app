8cc3a68a3deb84c8132769d855f78df458cb6aee
Senecal <ssenecal@una.edu>
2025-09-04 13:04:31 -0500

    mongo

--- app.mjs
+++ app.mjs
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 
 const app = express()
@@ -36,5 +37,30 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
 })
 
-// TODO: refactor to use any port
+
+const uri = "mongodb+srv://ssenecal:ssenecal@cluster0.gmyvepv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
